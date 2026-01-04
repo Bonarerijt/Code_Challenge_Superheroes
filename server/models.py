b63a8126b6173f2f86954204dbd58555ee5841be
Bonarerijt <prudyjj4@gmail.com>
2026-01-05 00:21:47 +0300

    Configured relationships between models

--- server/models.py
+++ server/models.py
@@ -1,7 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 # from sqlalchemy.orm import validates
-# from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 
 metadata = MetaData(naming_convention={
@@ -10,15 +10,22 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
-class Heroe(db.Model, SerializerMixin):
+
+class Hero(db.Model, SerializerMixin):
     __tablename__ = 'heroes'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     super_name = db.Column(db.String)
 
+    hero_powers = db.relationship(
+        'HeroPower', back_populates='hero', cascade='all, delete-orphan')
+    
+    powers = association_proxy("hero_powers", "power")
+
     def __repr__(self):
-        return f'<Heroe {self.id}, {self.name}: {self.super_name}>'
+        return f'<Hero {self.id}, {self.name}: {self.super_name}>'
+
 
 class Power(db.Model, SerializerMixin):
     __tablename__ = 'powers'
@@ -27,9 +34,15 @@ class Power(db.Model, SerializerMixin):
     name = db.Column(db.String)
     description = db.Column(db.String)
 
+    hero_powers = db.relationship(
+        'HeroPower', back_populates='power', cascade='all, delete-orphan')
+    
+    heroes = association_proxy('hero_powers', 'hero')
+
     def __repr__(self):
         return f'<Power ({self.id}) {self.name}; {self.description}>'
-    
+
+
 class HeroPower(db.Model, SerializerMixin):
     __tablename__ = 'hero_powers'
 
@@ -38,6 +51,9 @@ class HeroPower(db.Model, SerializerMixin):
 
     hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'))
     power_id = db.Column(db.Integer, db.ForeignKey('powers.id'))
+
+    hero = db.relationship('Hero', back_populates='hero_powers')
+    power = db.relationship('Power', back_populates='hero_powers')
 
     def __repr__(self):
         return f'<HeroPower ({self.id}) {self.strength}>'
